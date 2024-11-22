7e16cc46065fd4972e39016dc6e8dd84f4625a06
Maliina9 <Maliina9991@outlook.com>
2024-11-22 13:56:37 +0100

    ddd
    ddd

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const textFields = document.querySelectorAll('.textfield');
 const knapp = document.getElementsByClassName('knapp');
 const output = document.getElementById('output');
 
-function example1(color, content) {
-    console.log('function example1(color, content' , color + content);
-    return 'Returnerar' + color + '' + content;
-    }
-
-console.log(example1('color', 'content'));
+function uppdate(e){
+    console.log('Function: e', e.target);
+    const inputName = e.target.name;
+    console.log('Name-attribut:', inputName);
+    if (inputName === 'content') {
+    output.innerHTML = e.target.value;
+}}

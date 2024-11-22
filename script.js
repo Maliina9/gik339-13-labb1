1f64b494ab7bd86c654d97fae515bb4ce3c21954
Maliina9 <Maliina9991@outlook.com>
2024-11-22 14:07:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ function uppdate(e){
     if (inputName === 'content') {
     output.innerHTML = e.target.value;
 }}
+
+// Lägg till eventlyssnare till textfälten (color och content)
+color.addEventListener('input', uppdate);
+content.addEventListener('input', uppdate);
+
+// Eventlyssnare för checkboxen
+checkbox.addEventListener('change', function() {
+    // Ändra bakgrundsfärg på div-elementet till den färg som anges i color-input
+    output.style.backgroundColor = color.value;
+    console.log('Checkbox status:', Checkbox.checked);
+});
+
+// Eventlyssnare för knappen
+knapp.addEventListener('click', function() {
+    // Ta bort div-elementet vid klick
+    output.style.display = 'none';
+});

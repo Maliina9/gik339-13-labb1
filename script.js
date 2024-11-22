a03e83f16baf8f3e31b8e0238f0832c744bab115
Maliina9 <Maliina9991@outlook.com>
2024-11-22 13:24:03 +0100

    gg
    kjj

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const checkbox = document.querySelector('#divStyle');
 const textFields = document.querySelectorAll('.textfield');
+const knapp = document.getElementsByClassName('knapp');
+const output = document.getElementById('output');
+
+function example1(color, content) {
+    console.log('function example1(color, content' , color + content);
+    return 'Returnerar' + color + '' + content;
+    }
+
+console.log(example1('color', 'content'));
